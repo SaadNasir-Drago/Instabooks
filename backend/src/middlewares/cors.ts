48032cc6f894c9c6a1a041d9ba6827d7f7691b91
SaadNasir-Drago <snasir2194@gmail.com>
2024-11-01 19:14:25 +0600

    testing

--- backend/src/middlewares/cors.ts
+++ backend/src/middlewares/cors.ts
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://instabooks-livid.vercel.app'],
   optionsSuccessStatus: 204,
   methods: 'GET, POST, PUT, DELETE',
   credentials: true // Allows cookies to be sent
